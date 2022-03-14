208583d319384e2027ff9aaddd86b6ccd826e894
unknown <rubendanielconrad@gmail.com>
2022-03-14 19:01:15 -0300

    Se agrega carrito, se cambian estilos, se agrega background y se organizan elementos

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../img/white-logo.png'
+
+
+const Logo = () => {
+    return( 
+        <img src={logo} width="100" height="100" className="d-inline-block align-top logo-color" alt="Logo Ruso Bier" />
+)};
+
+export default Logo;
--- src/components/NavbarD.js
+++ src/components/NavbarD.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
-import logo from '../img/logo.png'
+import {Navbar, Container, Nav, Button} from 'react-bootstrap';
+import CartIcon from './CartIcon';
+import Logo from './Logo';
 
 
 const NavbarD = () => {
     return( 
-    <Navbar bg="light" expand="lg">
+    <Navbar className='nav-back-color' expand="lg">
         <Container>
           <Navbar.Brand href="#home">
-             <img src={logo} width="100" height="100" className="d-inline-block align-top" alt="React Bootstrap logo" />
+             <Logo />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className='flex-grow-0'>
             <Nav className="me-auto">
-              <Nav.Link href="#home" className='fs-4'>Productos</Nav.Link>
-              <Nav.Link href="#link" className='fs-4'>Servicios</Nav.Link>
-              <Nav.Link href="#link" className='fs-4'>Galería</Nav.Link>
+              <Nav.Link href="#home" className='fs-4 underline text-color-standard'>Productos</Nav.Link>
+              <Nav.Link href="#link" className='fs-4 underline text-color-standard'>Servicios</Nav.Link>
+              <Nav.Link href="#link" className='fs-4 underline text-color-standard'>Galería</Nav.Link>
             </Nav>
           </Navbar.Collapse>
+          <Button variant="outline-secondary" className='border-0'>
+            <CartIcon />
+          </Button>
         </Container>
       </Navbar>
 )};
