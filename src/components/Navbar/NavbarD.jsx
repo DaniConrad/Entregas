07eb43c722476c4d7a690dccd6aac84852385951
unknown <rubendanielconrad@gmail.com>
2022-03-29 19:21:26 -0300

    Se agrega Filter

---
+++ src/components/Navbar/NavbarD.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CartIcon from '../icons/CartIcon';
+import Logo from '../icons/Logo';
+
+
+const NavbarD = () => {
+    return( 
+      <header className="App-header">
+          <Navbar className='nav-back-color' expand="lg">
+              <Container>
+                <Navbar.Brand as={Link} to="/">
+                  <Logo />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className='flex-grow-0'>
+                  <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/products" className='fs-4 underline text-color-standard'>Productos</Nav.Link>
+                    <Nav.Link as={Link} to="/services" className='fs-4 underline text-color-standard'>Servicios</Nav.Link>
+                    <Nav.Link as={Link} to="/contact" className='fs-4 underline text-color-standard'>Contacto</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+                <CartIcon />
+              </Container>
+            </Navbar>
+      </header>
+)};
+    
+// le pasan un nav.link as={Link} para los link de router
+
+
+
+export default NavbarD;
