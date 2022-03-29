7d9851f2f0d97fcb969e333987a934cbdf8a6275
unknown <rubendanielconrad@gmail.com>
2022-03-29 19:56:35 -0300

    Se agrega Footer

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ import ItemDetailContainer from './components/Detail/ItemDetailContainer';
 import Filter from './components/Filter/Filter'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Error404 } from './components/Error 404/Error404';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div >
+      
+        <BrowserRouter>
+        <div className="App">
           <NavbarD />
-          <Filter />
-          <Routes>
-            <Route path='/' element={<ItemListContainer />}  />
-            <Route path='/products' element={<ItemListContainer />} />
-            <Route path='/category/:categoryId' element={<ItemListContainer />} />
-            <Route path='/detail/:itemId' element={ <ItemDetailContainer/> } />
+            <Filter />
+            <Routes>
+              <Route path='/' element={<ItemListContainer />}  />
+              <Route path='/products' element={<ItemListContainer />} />
+              <Route path='/category/:categoryId' element={<ItemListContainer />} />
+             <Route path='/detail/:itemId' element={ <ItemDetailContainer/> } />
 
-          <Route path='*' element={ <Error404 /> } />
-            </Routes>
-      </BrowserRouter>
+           <Route path='*' element={ <Error404 /> } />
+             </Routes>
+             </div>
+             <Footer />
+
+          </BrowserRouter>
+      
+      
     </div>
+    
   );
 }
 
